95e9f268c2b02b77893c866ea57d3cb8901b3bd6
Kangooboy <kangooboy@gmail.com>
2022-07-12 21:51:19 +0300

    add routing

---
+++ project/src/const.ts
@@ -0,0 +1,11 @@
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+export const PROMO_FILM = {
+  name: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  date: 2014
+};
